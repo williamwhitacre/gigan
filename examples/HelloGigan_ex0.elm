170034a84124b1bd387420871577d09c8b75f088
William Whitacre <wgwhitacre1@cougars.ccis.edu>
2016-02-22 04:03:02 -0600

    commit clean example 0

--- examples/HelloGigan_ex0.elm
+++ examples/HelloGigan_ex0.elm
@@ -11,6 +11,9 @@ excercised.
 import Gigan.Core exposing (..)
 import Html exposing (Html, div, span, button, text)
 import Html.Events exposing (onClick)
+import Html.Attributes exposing (style)
+
+import Window
 
 import Signal
 import Task exposing (Task)
@@ -19,22 +22,36 @@ import Time exposing (Time)
 
 
 type Action =
-  Increment
-  | Decrement
-  | Stop
-  | Auto Int
+  Change Int
+  | AutoOn Int
+  | AutoDo
+  | AutoOff
+  | CurrentWidth Int
 
 
 type alias Model =
   { stop : Bool
   , auto : Int
   , value : Int
+  , width : Int
   }
 
 
-autoIncrement : Int -> OrbiterTask () Action
-autoIncrement amount =
-  orbiterBlindAgent (orbiterAgentSuccess [Auto amount]) (Task.sleep 1000)
+autoIncrement : Model -> TaskDispatchment () Action
+autoIncrement model =
+  if not model.stop && model.auto /= 0 then
+    dispatchTasks [orbiterBlindAgent (orbiterAgentSuccess [AutoDo]) (Task.sleep 1000)]
+  else
+    dispatchTasks []
+
+
+styleOut : Html.Attribute
+styleOut =
+  style
+    [ ("position", "absolute")
+    , ("top", "20px")
+    , ("left", "20px")
+    ]
 
 
 model0 : Model
@@ -42,25 +59,40 @@ model0 =
   { stop = False
   , auto = 0
   , value = 0
+  , width = 0
   }
+
+
+autoActions : Model -> Int -> List Action
+autoActions model amount =
+  [if model.auto == 0 then AutoOn amount else AutoOff]
+
+
+stopOr : Model -> String -> String
+stopOr model say =
+  if model.auto /= 0 && not model.stop then "stop" else say
 
 
 present : Signal.Address (List Action) -> Time -> Model -> ViewOutput Action Html ()
 present address now model =
-  div []
-    [ button
-      [ onClick address [if model.auto /= 0 then Stop else Auto -1] ]
-      [ text (if model.auto /= 0 then "stop" else "--") ]
+  div
+    [ styleOut ]
+    [ div
+        [ ]
+        [ button [ onClick address (autoActions model -1) ] [ text (stopOr model "--") ]
+        , button [ onClick address [Change -1] ]            [ text "-" ]
+        , button [ onClick address [Change 1] ]             [ text "+" ]
+        , button [ onClick address (autoActions model 1) ]  [ text (stopOr model "++") ]
 
-    , button [ onClick address [Decrement] ] [ text "-" ]
-
-    , span [] [ text (toString model.value) ]
-
-    , button [ onClick address [Increment] ] [ text "+" ]
-
-    , button
-      [ onClick address [if model.auto /= 0 then Stop else Auto 1] ]
-      [ text (if model.auto /= 0 then "stop" else "++") ]
+        , span [] [ text (toString model.value) ]
+        ]
+    , div
+        [ ]
+        [ Html.hr [] []
+        , text ((++) "window width last changed to " <| toString model.width)
+        , Html.br [] []
+        , text ((++) "the timestamp of the last executed action was " <| toString now)
+        ]
     ]
 
   |> presented
@@ -68,28 +100,37 @@ present address now model =
 
 update : Action -> Time -> Model -> UpdatedModel Action Model ()
 update action now model =
-  let
-    model' =
-      case action of
-        Increment -> { model | value = model.value + 1 }
-        Decrement -> { model | value = model.value - 1 }
+  case action of
+    CurrentWidth width ->
+      { model | width = width }
+      |> updated
 
-        Stop ->
-          if not model.stop then { model | stop = True, auto = 0 } else model
+    Change amount ->
+      { model | value = model.value + amount }
+      |> updated
 
-        Auto n ->
-          if model.stop then { model | stop = False, auto = 0 }
-          else { model | auto = n, value = model.value + n }
+    AutoOn amount ->
+      { model | stop = False, auto = amount }
+      |> \model' -> updated model'
+      |> withDispatchment (autoIncrement model')
 
-  in
-    updated model'
-    |> withTasks (if model'.auto /= 0 then [autoIncrement model'.auto] else [ ])
+    AutoDo ->
+      update (Change model.auto) now model
+      |> withDispatchment (autoIncrement model)
+
+    AutoOff ->
+      { model | stop = True, auto = 0 }
+      |> updated
+
 
 
 output : OrbiterOutput Action Model Html ()
 output =
   (defProgram present update model0)
-    `orbitsWithWork` nilTask
+    `withSequenceInputs`
+      [ Signal.map (CurrentWidth >> flip (::) []) Window.width ]
+    `orbitsWithWork`
+      nilTask
     +--> itself
 
 
